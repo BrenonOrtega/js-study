c9efba4aa187748ca51d20846d4c6f95008a88ac
Brenon Ortega <bortega@cpqi.com>
2021-07-06 10:25:33 -0300

    makes PostgresDriver an IIFE returning a connection.

--- ew-node-api/08-multitenancy-postgres/src/database/repositories/strategies/postgres/postgresDriver.js
+++ ew-node-api/08-multitenancy-postgres/src/database/repositories/strategies/postgres/postgresDriver.js
@@ -1,20 +1,22 @@
-import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+import * as dotenv from 'dotenv';
 
 
 const PostgresDriver = (function() {
     dotenv.config();
-    return new Sequelize(
+
+    var driver = new Sequelize(
         process.env.POSTGRES_DATABASE,
         process.env.POSTGRES_USER,
         process.env.POSTGRES_PASSWORD, 
         {
             host: process.env.POSTGRES_HOST,
-            port: process.env.POTGRES_PORT,
+            port: Number(process.env.POTGRES_PORT),
             dialect: 'postgres',
-            quoteIdentifier:false,
-            operatorAliases:false,
+            quoteIdentifiers:false,
         });
+
+    return driver;
 })();
 
 export default PostgresDriver;
